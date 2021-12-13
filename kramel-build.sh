972afa9fe6bd8e6a1e8f7b00b0b2e513bd59604d
Cyber Knight <cyberknight755@gmail.com>
2021-12-13 17:11:53 +0800

    kramel-build: switched CHATID
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kramel-build.sh
+++ kramel-build.sh
@@ -84,7 +84,7 @@ PTTG=1
 	if [ $PTTG = 1 ]
 	then
 		# Set Telegram Chat ID
-		CHATID="-1001361882613"
+		CHATID="607425846"
 	fi
 
 # Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
